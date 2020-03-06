541c6de43f28a6e0f34afacf985321cfd1a939c9
Sunil Maharjan <ratnasunil70@gmail.com>
2020-03-06 22:41:30 +0200

    Updates

---
+++ precache-manifest.313239d9691787430f81127c546e05fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d89f9a72d8b89978577b4c9a55d11ead",
+    "url": "/picsearch/index.html"
+  },
+  {
+    "revision": "7914a04c3dbbab4ac7be",
+    "url": "/picsearch/static/css/main.465765bc.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/picsearch/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "7914a04c3dbbab4ac7be",
+    "url": "/picsearch/static/js/main.abbf8487.chunk.js"
+  },
+  {
+    "revision": "accebfa7be923973ce51",
+    "url": "/picsearch/static/js/runtime~main.a4e6d723.js"
+  }
+]);
